8620c475e6a946ef5fbe04e6d322ae2f2408fcac
Jeee3T <prasanjeetpanda06@gmail.com>
2022-12-26 20:52:58 +0530

    Initial Commit

---
+++ attendance_system/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# def homePage(request):
+#     return render (request, "index.html")
+
+
+def homePage(request):
+    return HttpResponse("Welcome to the Homepage ")
